d9ed055c48e51409910685ca0f83b45d4f87dc33
Ben Frederickson <github@benfrederickson.com>
2017-01-22 10:48:31 -0800

    fix travis

--- implicit/utils.py
+++ implicit/utils.py
@@ -14,5 +14,5 @@ def check_open_blas():
     """ checks to see if using OpenBlas. If so, warn if the number of threads isn't set to 1
     (causes perf issues) """
     if np.__config__.get_info('openblas_info') and os.environ.get('OPENBLAS_NUM_THREADS') != '1':
-        logging.warn("OpenBLAS detected. Its highly recommend to set the environment variable "
-                     "'export OPENBLAS_NUM_THREADS=1' to disable its internal multithreading")
+        logging.warning("OpenBLAS detected. Its highly recommend to set the environment variable "
+                        "'export OPENBLAS_NUM_THREADS=1' to disable its internal multithreading")
